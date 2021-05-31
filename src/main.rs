afc4a26fb0577d173414ee11c7de707a074f8682
Aleksandr Schemelev <prophreaker@gmail.com>
2021-05-31 15:30:20 +0300

    feat: add simple route handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, I\"m Rust")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8080");
+    HttpServer::new(|| {
+        App::new().route("/hello", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8080") ?
+    .run()
+    .await
 }
